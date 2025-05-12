1f5353ad0eceac494fd598e6766ef00ab41eb59d
cmariofonseca <cmariofonseca@gmail.com>
2025-05-12 13:27:12 +0000

    feat(HU0002): Set up the base for the login form component

---
+++ src/app/auth/page.tsx
@@ -0,0 +1,9 @@
+import Login from "../components/login";
+
+export default function AuthPage() {
+    return (
+        <main className="flex flex-col items-center justify-center min-h-screen bg-slate-100">
+            <Login />
+        </main>
+    );
+}
