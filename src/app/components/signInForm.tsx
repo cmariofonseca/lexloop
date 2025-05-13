6926d5f45f53fbb214ae0d43f24816fc2640b712
Fonseca Naranjo Carlos <Carlos.Fonseca+Suracl@externos.afpcapital.cl>
2025-05-12 20:10:03 -0500

    chore(HU0004): Configure Firebase Hosting and deploy static build

--- src/app/components/signInForm.tsx
+++ src/app/components/signInForm.tsx
@@ -1,62 +1,67 @@
-'use client';
+"use client";
 
-import { useState } from 'react';
-import { signInWithEmailAndPassword } from 'firebase/auth';
+import { useState } from "react";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { FirebaseError } from "firebase/app";
 
-import { auth } from '@/app/libs/firebase';
+import { auth } from "@/app/libs/firebase";
 
 export default function SignInForm() {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [error, setError] = useState('');
-    const [loading, setLoading] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
 
-    const handleSubmit = async (e: React.FormEvent) => {
-        e.preventDefault();
-        if (!email.trim() || !password.trim()) return;
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    if (!email.trim() || !password.trim()) return;
 
-        setLoading(true);
-        setError('');
+    setLoading(true);
+    setError("");
 
-        try {
-            const userCredential = await signInWithEmailAndPassword(auth, email, password);
-            const user = userCredential.user;
-            console.log('Logged in:', user.email);
-        } catch (err: any) {
-            console.error(err);
-            setError(err.message);
-        } finally {
-            setLoading(false);
-        }
-    };
+    try {
+      const userCredential = await signInWithEmailAndPassword(auth, email, password);
+      const user = userCredential.user;
+      console.log("Logged in:", user.email);
+    } catch (err) {
+      const error = err as FirebaseError;
+      console.error(error);
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
 
-    return (
-        <form onSubmit={handleSubmit} className="relative bg-white max-w-sm w-full h-full p-6 rounded-2xl shadow-lg flex flex-col justify-center items-center">
-            <input
-                type="email"
-                placeholder="Email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                className="w-full mb-2 p-2 border rounded"
-                required
-            />
-            <input
-                type="password"
-                placeholder="Password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                className="w-full mb-2 p-2 border rounded"
-                required
-            />
-            {error && <p className="text-red-600 text-sm mb-2">{error}</p>}
-            
-            <button
-            type="submit"
-                disabled={loading}
-                className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-            >
-                {loading ? 'Logging in...' : 'Login'}
-            </button>
-        </form>
-    );
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className="relative bg-white max-w-sm w-full h-full p-6 rounded-2xl shadow-lg flex flex-col justify-center items-center"
+    >
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        className="w-full mb-2 p-2 border rounded"
+        required
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        className="w-full mb-2 p-2 border rounded"
+        required
+      />
+      {error && <p className="text-red-600 text-sm mb-2">{error}</p>}
+
+      <button
+        type="submit"
+        disabled={loading}
+        className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+      >
+        {loading ? "Logging in..." : "Login"}
+      </button>
+    </form>
+  );
 }
