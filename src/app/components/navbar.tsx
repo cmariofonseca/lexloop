b164efd8dc89f6b1937cded189ab223d9181dd44
Fonseca Naranjo Carlos <Carlos.Fonseca+Suracl@externos.afpcapital.cl>
2025-05-12 20:13:06 -0500

    Actualizacion de versionado

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,22 +1,37 @@
-'use client';
-import Link from 'next/link';
-import { usePathname } from 'next/navigation';
-import { LogIn, PlusSquare, Square } from 'lucide-react';
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { LogIn, PlusSquare, Square } from "lucide-react";
 
 export default function Navbar() {
-    const pathname = usePathname();
+  const pathname = usePathname();
 
-    return (
-        <nav className="w-full bg-white border-b shadow-sm z-10 flex justify-around items-center h-full">
-            <Link href="/pages/cards" className={pathname === '/pages/cards' ? 'text-blue-600' : 'text-gray-400'}>
-                <Square size={24} />
-            </Link>
-            <Link href="/pages/add" className={pathname === '/pages/add' ? 'text-blue-600' : 'text-gray-400'}>
-                <PlusSquare size={24} />
-            </Link>
-            <Link href="/pages/auth" className={pathname === '/pages/auth' ? 'text-blue-600' : 'text-gray-400'}>
-                <LogIn size={24} />
-            </Link>
-        </nav>
-    );
+  return (
+    <nav className="w-full bg-white border-b shadow-sm z-10 flex justify-around items-center h-full">
+      <Link
+        href="/pages/cards"
+        className={pathname === "/pages/cards" ? "text-blue-600" : "text-gray-400"}
+      >
+        <Square size={24} />
+      </Link>
+      <Link
+        href="/pages/add"
+        className={pathname === "/pages/add" ? "text-blue-600" : "text-gray-400"}
+      >
+        <PlusSquare size={24} />
+      </Link>
+      <Link
+        href="/pages/auth"
+        className={pathname === "/pages/auth" ? "text-blue-600" : "text-gray-400"}
+      >
+        <LogIn size={24} />
+      </Link>
+      <Link
+        href="/pages/cards"
+        className={pathname === "/pages/cards" ? "text-blue-600" : "text-gray-400"}
+      >
+        <small>v:1.10.0</small>
+      </Link>
+    </nav>
+  );
 }
